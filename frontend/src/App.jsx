f6e1b953a4734634bb4806ea40a7967b7282689b
alokmishra <alok.230101016@iiitbh.ac.in>
2024-10-20 04:16:38 +0530

    error resolveddd11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import React, { Suspense, lazy } from 'react';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
-
-// Use React.lazy for dynamic imports
-const Home = lazy(() => import("./pages/Home/Home"));
-const Login = lazy(() => import("./pages/Login/Login"));
-const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
 
 function App() {
   const { authUser } = useAuthContext();
@@ -14,13 +11,11 @@ function App() {
   return (
     <>
       <div className="p-4 h-screen flex justify-center items-center">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
-            <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
-            <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
+        </Routes>
         <Toaster />
       </div>
     </>
