ba60285a167b018148fa505dc666f88eb6dacb31
alokmishra <alok.230101016@iiitbh.ac.in>
2024-10-21 13:08:31 +0530

    change made

--- frontend/src/Components/messages/MessageContainer.jsx
+++ frontend/src/Components/messages/MessageContainer.jsx
@@ -1,61 +1,36 @@
-import { useEffect } from "react";
-import useConversation from "../../Zustand/useConversation";
-import MessageInput from "./MessageInput";
-import Messages from "./Messages";
-import { TiMessages } from "react-icons/ti";
-import { IoBackspaceSharp } from "react-icons/io5";
-
 const MessageContainer = () => {
-  const { selectedConversation, setSelectedConversation } = useConversation();
-
-  useEffect(() => {
-    return () => setSelectedConversation(null);
-  }, [setSelectedConversation]);
-
-  return (
-    <div className="flex flex-col h-full w-full md:min-w-[450px]">
-      {!selectedConversation ? (
-        <NoChatSelected />
-      ) : (
-        <>
-        
-          <div className="bg-slate-500 px-4 py-2 flex items-center justify-between mb-2">
-            <span className="text-gray-900 font-bold">
-              {selectedConversation.fullName}
-            </span>
-            <IoBackspaceSharp
-              className="w-6 h-6 text-white cursor-pointer"
-              onClick={() => setSelectedConversation(null)}
-            />
-          </div>
-
-    
-          <div className="flex-grow overflow-y-auto">
-            <Messages />
-          </div>
-
-         
-          <div className="mt-2">
-            <MessageInput />
-          </div>
-        </>
-      )}
-    </div>
-  );
-};
-
-const NoChatSelected = () => {
-  const authUser = { fullName: "User" };
-
-  return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 {authUser.fullName} ❄</p>
-        <p>Select a chat to start messaging</p>
-        <TiMessages className="text-3xl md:text-6xl text-center" />
+    const { selectedConversation, setSelectedConversation } = useConversation();
+  
+    useEffect(() => {
+      return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
+  
+    return (
+      <div className="flex flex-col h-full w-full md:min-w-[450px]">
+        {!selectedConversation ? (
+          <NoChatSelected />
+        ) : (
+          <>
+            <div className="bg-slate-500 px-4 py-2 flex items-center justify-between mb-2">
+              <span className="text-gray-900 font-bold">
+                {selectedConversation.fullName}
+              </span>
+              <IoBackspaceSharp
+                className="w-6 h-6 text-white cursor-pointer"
+                onClick={() => setSelectedConversation(null)}
+              />
+            </div>
+  
+            <div className="flex-grow overflow-y-auto">
+              <Messages />
+            </div>
+  
+            <div className="mt-2">
+              <MessageInput />
+            </div>
+          </>
+        )}
       </div>
-    </div>
-  );
-};
-
-export default MessageContainer;
+    );
+  };
+  
--- frontend/src/Components/messages/Messages.jsx
+++ frontend/src/Components/messages/Messages.jsx
@@ -8,34 +8,31 @@ import useListenMessages from "../../Hooks/useListenMessages";
 
 const Messages = () => {
 	const { messages, loading } = useGetMessages();
-
-	useListenMessages()
-	//console.log(messages)
-	// useListenMessages();
+	useListenMessages();
 	const lastMessageRef = useRef();
-
+  
 	useEffect(() => {
-		setTimeout(() => {
-			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
-		}, 100);
+	  setTimeout(() => {
+		lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+	  }, 100);
 	}, [messages]);
-
+  
 	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			{!loading &&
-				messages.length > 0 &&
-				messages.map((message) => (
-					<div key={message._id} ref={lastMessageRef}>
-						<Message message={message} />
-					</div>
-				))}
-
-			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-			{!loading && messages.length === 0 && (
-				<p className='text-center'>Send a message to start the conversation</p>
-			)}
-		</div>
+	  <div className='px-4 flex-1 overflow-auto h-full'>
+		{!loading &&
+		  messages.length > 0 &&
+		  messages.map((message) => (
+			<div key={message._id} ref={lastMessageRef}>
+			  <Message message={message} />
+			</div>
+		  ))}
+  
+		{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+		{!loading && messages.length === 0 && (
+		  <p className='text-center'>Send a message to start the conversation</p>
+		)}
+	  </div>
 	);
-};
+  };
 export default Messages;
 
