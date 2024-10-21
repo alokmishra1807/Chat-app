faf12a275f1e5648875713f18752954e3623f1f1
alokmishra <alok.230101016@iiitbh.ac.in>
2024-10-21 09:33:05 +0530

    small screen problem resolved

--- frontend/src/Components/Sidebar/Conversation.jsx
+++ frontend/src/Components/Sidebar/Conversation.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from 'react';
 import useConversation from '../../Zustand/useConversation';
 import { useSocketContext } from '../../context/soketContext';
-const Conversation = ({conversation,lastIdx,emoji}) => {
-const {onlineUsers} = useSocketContext();
 
-const isOnline = onlineUsers.includes(conversation._id)
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { onlineUsers } = useSocketContext();
 
-const {selectedConversation,setSelectedConversation} = useConversation()
+  const isOnline = onlineUsers.includes(conversation._id);
 
-const isSelected = selectedConversation?._id === conversation._id;
- 
-	return (
-		<>
-			<div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected?"bg-sky-500":""}`}
-			onClick={() => {
-				//console.log('Selected Conversation:', conversation);
-				setSelectedConversation(conversation);
-			}}>
-				<div className={`avatar ${isOnline? "online":""}`}>
-					<div className='w-12 rounded-full'>
-						<img
-							src={conversation.profilePic}
-							alt='user avatar'
-						/>
-					</div>
-				</div>
+  const { selectedConversation, setSelectedConversation } = useConversation();
 
-				<div className='flex flex-col flex-1'>
-					<div className='flex gap-3 justify-between'>
-						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
-						<span className='text-xl'>{emoji}</span>
-					</div>
-				</div>
-			</div>
+  const isSelected = selectedConversation?._id === conversation._id;
 
-			{!lastIdx && <div className='divider my-0 py-0 h-1' />}
-		</>
-	);
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer 
+          ${isSelected ? 'bg-sky-500' : ''} 
+          ${selectedConversation && !isSelected ? 'hidden sm:flex' : 'flex'}`} // Show only selected conversation on small screens
+        onClick={() => {
+          setSelectedConversation(conversation);
+        }}
+      >
+        <div className={`avatar ${isOnline ? 'online' : ''}`}>
+          <div className='w-12 rounded-full'>
+            <img src={conversation.profilePic} alt='user avatar' />
+          </div>
+        </div>
+
+        <div className='flex flex-col flex-1'>
+          <div className='flex gap-3 justify-between'>
+            <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
+          </div>
+        </div>
+      </div>
+
+      {!lastIdx && <div className='divider my-0 py-0 h-1' />}
+    </>
+  );
 };
-export default Conversation
+
+export default Conversation;
