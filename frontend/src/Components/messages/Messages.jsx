cd01338bdf2b2bbbdc8dae7304477cd7fcf9e2de
alokmishra <alok.230101016@iiitbh.ac.in>
2024-10-22 02:17:15 +0530

    Hook resolved

--- frontend/src/Components/messages/Messages.jsx
+++ frontend/src/Components/messages/Messages.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import useGetMessages from "../../hooks/useGetMessages";
+import useGetMessages from "../../Hooks/useGetMessages";
 import MessageSkeleton from "../Skeleton/MessageSkeleton";
 import Message from "./message";
 import useListenMessages from "../../hooks/useListenMessages";
